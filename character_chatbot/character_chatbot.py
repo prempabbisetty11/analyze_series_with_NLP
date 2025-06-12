6e574ad2f39d6932c9e8003b13be127ea4d293fa
PREM <premsai620@outlook.com>
2025-06-12 21:37:50 +0530

    Update project files

--- character_chatbot/character_chatbot.py
+++ character_chatbot/character_chatbot.py
@@ -11,7 +11,7 @@ from transformers import (
 from peft import LoraConfig, PeftModel
 from trl import SFTConfig, SFTTrainer
 import transformers
-
+import gc
 
 # Remove Action From Transcript
 def remove_parantheses(text):
@@ -25,7 +25,6 @@ class CharacterChatbot():
                  data_path="/content/data/naruto.csv",
                  huggingface_token=None
                  ):
-        
         self.model_path = model_path
         self.data_path = data_path
         self.huggingface_token = huggingface_token
@@ -35,14 +34,13 @@ class CharacterChatbot():
         if self.huggingface_token is not None:
             huggingface_hub.login(self.huggingface_token)
 
-
         if huggingface_hub.repo_exists(self.model_path):
             self.model = self.load_model(self.model_path)
         else:
             print("Model not found in huggingface hub we will train our own model")
             train_dataset = self.load_data()
             self.train(self.base_model_path, train_dataset)
-            self.model=self.load_model(self.model_path)
+            self.model = self.load_model(self.model_path)
 
     def chat(self, message, history):
         messages = []
@@ -55,21 +53,19 @@ class CharacterChatbot():
 
         messages.append({"role": "user", "content": message})
 
-        terminator = [
-            self.model.tokenizer.eos_token_id,
-            self.model.tokenizer.convert_tokens_to_ids("<|eot_id|>")
-        ]
+        # Use only eos_token_id, not a list
+        terminator = self.model.tokenizer.eos_token_id
 
         output = self.model(
             messages,
             max_length=256,
             eos_token_id=terminator,
-            do_sampe=True,
+            do_sample=True,
             temperature=0.6,
             top_p=0.9
         ) 
 
-        output_message = output[0]['generated_text'][-1]
+        output_message = output[0]['generated_text']
         return output_message       
 
     def load_model(self, model_path):
@@ -79,10 +75,10 @@ class CharacterChatbot():
             bnb_4bit_compute_dtype=torch.float16
         )
         pipeline = transformers.pipeline("text-generation",
-                                            model=model_path,
-                                            model_kwargs={"torch_dtype": torch.float16,
-                                                          "quantization_config":bnb_config
-                                            }
+                                         model=model_path,
+                                         model_kwargs={"torch_dtype": torch.float16,
+                                                       "quantization_config": bnb_config
+                                         }
         )
         return pipeline
     
@@ -90,7 +86,7 @@ class CharacterChatbot():
     def train(self,
               base_model_name_or_path,
               dataset,
-              ourput_dir="./results",
+              output_dir="./results",
               per_device_train_batch_size=1,
               gradient_accumulation_steps=1,
               optim="paged_adamw_32bit",
@@ -102,7 +98,6 @@ class CharacterChatbot():
               warmup_ratio=0.3,
               lr_scheduler_type="constant"
     ):
-        
         bnb_config = BitsAndBytesConfig(
             load_in_4bit=True,
             bnb_4bit_quant_type="nf4",
@@ -120,7 +115,7 @@ class CharacterChatbot():
 
         lora_alpha = 16
         lora_dropout = 0.1
-        lora_r=64
+        lora_r = 64
 
         peft_config = LoraConfig(
             lora_alpha=lora_alpha,
@@ -131,7 +126,7 @@ class CharacterChatbot():
         )
 
         training_args = SFTConfig(
-            output_dir=ourput_dir,
+            output_dir=output_dir,
             per_device_train_batch_size=per_device_train_batch_size,
             gradient_accumulation_steps=gradient_accumulation_steps,
             optim=optim,
@@ -153,7 +148,7 @@ class CharacterChatbot():
             train_dataset=dataset,
             peft_config=peft_config,
             dataset_text_field="prompt",
-            max_seq_len= max_seq_len,
+            max_seq_len=max_seq_len,
             tokenizer=tokenizer,
             args=training_args
         )
@@ -185,24 +180,22 @@ class CharacterChatbot():
         del model, base_model
         gc.collect()
 
-
     def load_data(self):
         naruto_transcript_df = pd.read_csv(self.data_path)
         naruto_transcript_df = naruto_transcript_df.dropna()
         naruto_transcript_df['line'] = naruto_transcript_df['line'].apply(remove_parantheses)
         naruto_transcript_df['number_of_words'] = naruto_transcript_df['line'].str.strip().str.split(" ")
-        naruto_transcript_df['number_of_words'] = naruto_transcript_df['number_of_words'].apply(lambda x: len(x) )
+        naruto_transcript_df['number_of_words'] = naruto_transcript_df['number_of_words'].apply(lambda x: len(x))
         naruto_transcript_df['naruto_response_flag'] = 0
-        naruto_transcript_df.loc[(naruto_transcript_df['name'] == "Naruto")&(naruto_transcript_df['number_of_words'] > 5), 'naruto_response_flag'] = 1
+        naruto_transcript_df.loc[(naruto_transcript_df['name'] == "Naruto") & (naruto_transcript_df['number_of_words'] > 5), 'naruto_response_flag'] = 1
 
-        indexes_to_take = list(naruto_transcript_df[(naruto_transcript_df['naruto_response_flag']==1)& (naruto_transcript_df.index>0)].index)
+        indexes_to_take = list(naruto_transcript_df[(naruto_transcript_df['naruto_response_flag'] == 1) & (naruto_transcript_df.index > 0)].index)
 
-        system_prompt = """"You are a Naruto from the anime "Naruto". Your responses should reflect his personality and speech patterns \n"""
+        system_prompt = """You are a Naruto from the anime "Naruto". Your responses should reflect his personality and speech patterns \n"""
         prompts = []
         for ind in indexes_to_take:
             prompt = system_prompt
-
-            prompt += naruto_transcript_df.iloc[ind -1]['line']
+            prompt += naruto_transcript_df.iloc[ind - 1]['line']
             prompt += "\n"
             prompt += naruto_transcript_df.iloc[ind]['line']
             prompts.append(prompt)
@@ -211,4 +204,3 @@ class CharacterChatbot():
         dataset = Dataset.from_pandas(df)
 
         return dataset
-
